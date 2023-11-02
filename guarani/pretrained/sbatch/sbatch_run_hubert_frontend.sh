7fd5ed1f53bcc894972e167247d550b9c26a2169
Yash Mathur <mathuryash5@gmail.com>
2023-11-02 10:45:18 -0400

    Added Frozen Hubert Config

---
+++ guarani/pretrained/sbatch/sbatch_run_hubert_frontend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=train_gn_branchformer_frontend_hubert_old_config
+#SBATCH --mem=80GB
+#SBATCH --gres=gpu:A6000:1
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=babel-shared-long
+#SBATCH -o out/train_gn_branchformer_frontend_hubert_old_config.out
+bash runners/run_hubert_frontend.sh
