9b079a7d80a2b975f3e6d189eb4d6bf5fdd67164
Winnie Chang <winniechang88888@gmail.com>
2023-11-02 04:40:57 -0400

    Fix wandb names + add multilingual configs

--- quechua/multilingual/run_branchformer_bpe100_multilingual.sh
+++ quechua/multilingual/run_branchformer_bpe100_multilingual.sh
@@ -12,11 +12,11 @@ set -o pipefail
     --ngpu 1 \
     --gpu_inference true \
     --inference_nj 1 \
-    --use_lm false \
+    --use_lm true \
     --lang quy \
     --token_type bpe \
     --nbpe 100 \
-    --asr_config conf/train_asr_quechua_branchformer_bpe100.yaml \
+    --asr_config conf/train_asr_quechua_branchformer_bpe100_multilingual.yaml \
     --inference_args "--beam_size 10 --ctc_weight 0.3" \
     --train_set train \
     --valid_set dev \
@@ -24,5 +24,5 @@ set -o pipefail
     --bpe_train_text "data/train/text" \
     --lm_train_text "data/train/text" \
     --audio_format wav \
-    --expdir exp_quy_branchformer_bpe100 "$@" 
+    --expdir exp_quy_branchformer_bpe100_multilingual "$@" 
 
